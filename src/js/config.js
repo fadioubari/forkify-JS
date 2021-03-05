492a3c14642b978fae34908fd036272e1468382f
fadioubari <fadi.oubari@gmail.com>
2021-03-05 14:55:35 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e033367a-e6ff-48a0-9c96-58711d80ced5';
+export const MODAL_CLOSE_SEC = 2.55;
